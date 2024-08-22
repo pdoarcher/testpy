d9feb4b09c4973a6cb631ef9af2dd5e226b5990a
Peter Do <pdo@archer.com>
2024-08-22 10:45:27 -0700

    1st push

---
+++ variable.py
@@ -0,0 +1,16 @@
+# lets start using variables and data types 
+
+# dont have to type var to declare a variable
+
+a = 1 #int
+b = ("str")  # string
+c = 2.0 # float
+d = False # boolean *first letter must be caps cuz or else it considered a variable"
+
+#multiple assignments 
+
+x, y, name, is_cool = (1, 2.5, "str", True)
+
+print('x')
+
+
